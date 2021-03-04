d52a20cae23d9bc68d816720cee77095a5ecfac3
Jzhen123 <jiayuzheng01@gmail.com>
2021-03-04 15:25:22 -0500

    Wireframe Established

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let currentMainText = document.querySelector('.currentMainText')
+function currentView(){
+    const view = 0;
+
+    let mainText = ["I can read your mind", "Pick a number from 01 - 99", "Add both digits together to get a new number", "Subtract your new number from the original nubmer", "Placeholder for number list", "Placeholder for symbol"];
+    let buttonText = [];
+    let textUnder = [];
+    let brButton = [];
+
+    let currentMainText = mainText[0];
+
+    // if (view == 0) {
+
+    // }
+    
+}
+currentView();
